0ce7526e9390751c29213de3014340b3cf68637a
Shahan Gamage <gamageshahan5@gmail.com>
2026-03-16 23:26:03 +0530

    Update MyFirstTestCase to include login functionality and validation

--- tests/BasicTest.spec.js
+++ tests/BasicTest.spec.js
@@ -1,11 +1,16 @@
 const {test, expect}= require('@playwright/test');
 
 
-test('MyFirstTestCase',async ({browser})=>{
+test.only('MyFirstTestCase',async ({browser})=>{
     //Chrome 
     const context = await browser.newContext();
     const page = await context.newPage();
-    await page.goto('https://www.google.com/');
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    await page.locator('#username').fill('Shahan Gamage');
+    await page.locator("[type='password']").fill('Shahan Gamage');
+    await page.locator('#signInBtn').click();
+    console.log(await page.locator("[style*='block']").textContent());
+    await expect(page.locator("[style*='block']")).toContainText('Incorrect');
 
 
     });
