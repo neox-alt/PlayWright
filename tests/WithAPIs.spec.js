e9999b458ba5701b85df9a0d0e673018501dcdbe
Shahan Gamage <gamageshahan5@gmail.com>
2026-03-19 12:08:44 +0530

    Add test scripts and configurations for API order management and network interception

--- tests/WithAPIs.spec.js
+++ tests/WithAPIs.spec.js
@@ -32,7 +32,7 @@ test ("placetheorder",async({page})=>{
     await page.locator("tbody").waitFor();
     const rows = await page.locator("tbody tr");
     let count = await rows.count();
-
+s
     for (let i = 0; i < count; ++i) {
         const rowOrderId = await rows.nth(i).locator("th").textContent();
         if (response.orderId.includes(rowOrderId)) {
